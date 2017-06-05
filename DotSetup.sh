38f668bd34993b0ee3863d360ebdd7b67fc7ee36
John Warnes <johnwarnes@mail.weber.edu>
2017-06-05 15:10:23 -0600

    added a defualt licnence

--- DotSetup.sh
+++ DotSetup.sh
@@ -466,6 +466,21 @@ GetUserInfo()
     read -p "$RESET${BOLD}Enter $BLUE Email Address$RESET$BOLD Ex\"JohnD@mail.weber.edu\": $GREEN" email
     read -p "$RESET${BOLD}Enter $BLUE Oganization$RESET$BOLD Ex\"WSU\": $GREEN" org
     read -p "$RESET${BOLD}Enter $BLUE Company$RESET$BOLD Ex\"WSU\": $GREEN" com
+    read -p "$RESET${BOLD}Enter $BLUE Default License$RESET$BOLD (hit enter for default): $GREEN" license
+    if [ -z license ]; then
+        license = 'this program is free software: you can redistribute it and/or modify\n
+                   it under the terms of the GNU General Public License as published by\n
+                   the Free Software Foundation, either version 3 of the License, or\n
+                   (at your option) any later version.\n
+
+                   This program is distributed in the hope that it will be useful,\n
+                   but WITHOUT ANY WARRANTY; without even the implied warranty of\n
+                   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n
+                   GNU General Public License for more details.\n
+
+                   You should have received a copy of the GNU General Public License\n
+                   along with this program.  If not, see <http://www.gnu.org/licenses/>\n.'
+    fi
 }
 
 
@@ -515,7 +530,7 @@ SetMacro( 'EMAIL',        '%s' )
 SetMacro( 'ORGANIZATION', '%s' )
 SetMacro( 'COMPANY',      '%s' )
 SetMacro( 'COPYRIGHT',    'Copyright (c) |YEAR|, |AUTHOR|' )
-SetMacro( 'LICENSE',      'GNU General Public License' )
+SetMacro( 'LICENSE',      '%s' )
 
 § =============================================================
 §  Date and Time Format
