746b94059b045ea0d56195df1be41df4f589ca54
John Warnes <johnwarnes@mail.weber.edu>
2017-11-12 23:45:41 -0700

    Upgrade to support shorter cleaner output.. but still have use to see modules being loaded

--- shell/autorun.sh
+++ shell/autorun.sh
@@ -1,127 +1,170 @@
 #!/bin/bash
-#===============================================================================
-#
-#          FILE: autorun.sh
-#
-#         USAGE: ./autorun.sh
-#
-#   DESCRIPTION:
-#
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
-#        AUTHOR: John Warnes (jwarnes), johnwarnes@mail.weber.edu
-#  ORGANIZATION: WSU
-#       CREATED: 06/06/2017 04:35:35 PM
-#      REVISION:  ---
-#===============================================================================
+#=================================================================
+#  Revision  032
+#  Modified  Sunday, 12 November 2017
+#=================================================================
 
 
 #===============================================================================
 # Check if this has already been run {
 #===============================================================================
-if [[ $DOTFILESAUTO == 1 ]]; then
-    #echo "$RESET${GREEN}[dotfiles Already Loaded]$RESET"
-    return 0
-else
-
-if [[ -f $DOTFILES/scripts/colors.sh ]]; then
-    source $DOTFILES/scripts/colors.sh
-fi
-    export DOTFILESAUTO=1
-fi
+Colors()
+{
+    if [[ -f $DOTFILES/scripts/colors.sh ]]; then
+        source $DOTFILES/scripts/colors.sh
+    fi
+}
 # } ===
+
+
+#===============================================================================
+# Check if this has already been run {
+#===============================================================================
+RunCheck()
+{
+    if [[ $DOTFILESAUTO == 1 ]]; then
+        #echo "$RESET${GREEN}[dotfiles Already Loaded]$RESET"
+        echo 1
+    else
+        export DOTFILESAUTO=1
+        echo 0
+    fi
+}
+# } ===
+
 
 #===============================================================================
 # Detect OS {
 #===============================================================================
-
-if [[ -f $DOTFILES/scripts/detectOS ]]; then
-    source $DOTFILES/scripts/detectOS
-fi
-
+DetectOS()
+{
+    if [[ -f $DOTFILES/scripts/detectOS ]]; then
+        source $DOTFILES/scripts/detectOS
+    fi
+}
 # } ===
 
-printf "["
 
 #===============================================================================
 # Python Virtual Environments {
 #===============================================================================
+PythonVirtualEnvironments()
+{
+    if [[ -f /usr/local/bin/virtualenvwrapper.sh ]]; then
 
-if [[ -f /usr/local/bin/virtualenvwrapper.sh ]]; then
+        if ! [[ -d $HOME/.virtualenvs ]]; then
+            mkdir $HOME/.virtualenvs
+        fi
+        export WORKON_HOME=$HOME/.virtualenv
 
-    if ! [[ -d $HOME/.virtualenvs ]]; then
-        mkdir $HOME/.virtualenvs
+        # Set Dir for development director
+        if ! [[ -d $HOME/dev ]]; then
+            mkdir $HOME/dev
+        fi
+        export PROJECT_HOME=$HOME/dev
+
+        #what python to use
+        export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+
+        source /usr/local/bin/virtualenvwrapper.sh
+        printf "${RESET}${GREEN}Python Virt Wrapper,$RESET|"
+    else
+        printf "${RESET}${YELLOW}!! Python Virt Wrapper !!$RESET "
     fi
-    export WORKON_HOME=$HOME/.virtualenv
-
-    # Set Dir for development director
-    if ! [[ -d $HOME/dev ]]; then
-        mkdir $HOME/dev
-    fi
-    export PROJECT_HOME=$HOME/dev
-
-    #what python to use
-    export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
-
-    source /usr/local/bin/virtualenvwrapper.sh
-    printf "${RESET}${GREEN}Python Virt Wrapper,$RESET|"
-else
-    printf "${RESET}${YELLOW}!! Python Virt Wrapper !!$RESET "
-fi
-# } ===
+}
+#} ===
 
 
 #===============================================================================
 # Shell Aliases {
 #===============================================================================
-if [[ -f $DOTFILES/shell/shell_aliases ]]; then
-    source $DOTFILES/shell/shell_aliases
-fi
-# }
+ShellAliases()
+{
+    if [[ -f $DOTFILES/shell/shell_aliases ]]; then
+        source $DOTFILES/shell/shell_aliases
+    fi
+}
+# } ===
 
 
 #===============================================================================
 # Personal Aliases {
 #===============================================================================
-if [[ -f $DOTFILES/secure/personal_aliases ]]; then
-    source $DOTFILES/secure/personal_aliases
-fi
-# }
+PersonalAliases ()
+{
+    if [[ -f $DOTFILES/secure/personal_aliases ]]; then
+        source $DOTFILES/secure/personal_aliases
+    fi
+}
+# } ===
 
 
 #===============================================================================
 # OSX Bash Completion {
 #===============================================================================
-if [[ $OS == 'OSX' ]] && [[ $SHELL == '/bin/bash' ]]; then
-    if [ -f $(brew --prefix)/etc/bash_completion ]; then
-        source $(brew --prefix)/etc/bash_completion
-        printf "${RESET}${GREEN}OSX Bash Complete$RESET|"
-    else
-        printf "${RESET}${YELLOW}!! OSX Bash Completion !!$RESEET "
+OSXBashCompletion ()
+{
+    if [[ $OS == 'OSX' ]] && [[ $SHELL == '/bin/bash' ]]; then
+        if [ -f $(brew --prefix)/etc/bash_completion ]; then
+            source $(brew --prefix)/etc/bash_completion
+            printf "${RESET}${GREEN}OSX Bash Complete$RESET|"
+        else
+            printf "${RESET}${YELLOW}!! OSX Bash Completion !!$RESEET "
+        fi
     fi
-fi
-# }
-
+}
+#} ===
 
 
 #===============================================================================
 # Bash Git-Prompt {
 #===============================================================================
-if [[ $SHELL == '/bin/bash' ]]; then
-    if [[ -f $DOTFILES/shell/git-prompt.sh ]]; then
-        source $DOTFILES/shell/git-prompt.sh
+BashGit-Prompt ()
+{
+    if [[ $SHELL == '/bin/bash' ]]; then
+        if [[ -f $DOTFILES/shell/git-prompt.sh ]]; then
+            source $DOTFILES/shell/git-prompt.sh
 
-        export GIT_PS1_SHOWCOLORHINTS=1
-        #PROMPT_COMMAND='__git_ps1 "\u@\h:\w" "\\\$ "'
-        PROMPT_COMMAND='__git_ps1 "${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\\[\033[00m\]" "\\[\033[00m\]\$ "'
-        printf "${RESET}${GREEN}Bash git-Prompt$RESET"
-    else
-        printf "${RESET}${YELLOW}!! Bash git-Prompt !!$RESET"
+            export GIT_PS1_SHOWCOLORHINTS=1
+            #PROMPT_COMMAND='__git_ps1 "\u@\h:\w" "\\\$ "'
+            PROMPT_COMMAND='__git_ps1 "${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\\[\033[00m\]" "\\[\033[00m\]\$ "'
+            printf "${RESET}${GREEN}Bash git-Prompt$RESET"
+        else
+            printf "${RESET}${YELLOW}!! Bash git-Prompt !!$RESET"
+        fi
     fi
-fi
-# }
+}
+#} ===
 
-echo "]"
-echo "${RESET}DOTFILES${GREEN} Ready$RESET"
+
+#===============================================================================
+# main {
+#===============================================================================
+main()
+{
+    Colors
+    DetectOS
+
+    HASRUN=$(RunCheck)
+
+    if [[ $HASRUN ]]; then
+        printf "$RESET[dotfiles reload: "
+        BashGit-Prompt
+        echo "]"
+    else
+        printf "["
+        PythonVirtualEnvironments
+
+        ShellAliases
+        PersonalAliases
+
+        OSXBashCompletion
+        BashGit-Prompt
+
+        echo "]"
+        echo "${RESET}DOTFILES${GREEN} Ready$RESET"
+    fi
+
+}
+#} ===
+main "$@"     #remember to pass all command line args
