058043e1159d06d04a7bdb2c9c4c97f7c0abb1eb
John Warnes <johnw@gurutechnologies.net>
2022-08-18 18:42:51 -0600

    Bugfix `git stats-commits` alias that show basic repo stats Cleanup and refactor code, update code styles for current python Add .vscode/settings for IDE spellchecker

--- DotSetup.py
+++ DotSetup.py
@@ -53,8 +53,8 @@ def collect_system_data():
     """
     SYS_DATA["home"] = os.path.expanduser("~")
     SYS_DATA["nvim_config"] = BAR = os.environ.get("XDG_CONFIG_HOME", SYS_DATA["home"] + "/.config/nvim/")  # None
-    SYS_DATA["sdir"] = get_script_path()
-    SYS_DATA["sfile"] = str(__file__)
+    SYS_DATA["script_dir"] = get_script_path()
+    SYS_DATA["script_file"] = str(__file__)
     SYS_DATA["os_kind"] = os.name
     SYS_DATA["os"] = platform.system()
     SYS_DATA["os_release"] = platform.release()
@@ -79,8 +79,8 @@ def display_system_data(skipUser: bool = False):
     print("║ OS     : {:{pad}} ║".format(SYS_DATA["os"], pad=43))
     print("║ Release: {:{pad}} ║".format(SYS_DATA["os_release"], pad=43))
     print("╠═ VIM ════════════════════════════════════════════════╣")
-    print("║ Script File     : {:{pad}} ║".format(SYS_DATA["sfile"], pad=34))
-    print("║ Script Directory: {:{pad}} ║".format(SYS_DATA["sdir"], pad=34))
+    print("║ Script File     : {:{pad}} ║".format(SYS_DATA["script_file"], pad=34))
+    print("║ Script Directory: {:{pad}} ║".format(SYS_DATA["script_dir"], pad=34))
     print("║ Home Directory  : {:{pad}} ║".format(SYS_DATA["home"], pad=34))
     print("║ Current Version : {:{pad}} ║".format(SYS_DATA["vim_version"], pad=34))
     print("║ Required Version: {:{pad}} ║".format(SETTINGS["vim_required"], pad=34)) if not skipUser else False
@@ -100,19 +100,19 @@ def hasDependencies(skipUser: bool = False) -> bool:
         return dependencies
 
     if parse_version(SETTINGS["vim_required"]) > parse_version(SYS_DATA["vim_version"]):
-        print(" Warning: Vim version is below the required version")
+        print("Warning: Vim version is below the required version")
     else:
-        print(" Vim version: OK")
+        print("Vim version: OK")
     if parse_version(SETTINGS["nvim_required"]) > parse_version(SYS_DATA["nvim_version"]):
         print(" Warning: Neovim version is below the recommend version")
     else:
-        print(" Neovim version: OK")
+        print("Neovim version: OK")
 
     if parse_version(SETTINGS["vim_required"]) > parse_version(SYS_DATA["vim_version"]) and parse_version(
         SETTINGS["nvim_required"]
     ) > parse_version(SYS_DATA["nvim_version"]):
         dependencies = False
-        print(" ERROR: Vim or Neovim must be above the required version")
+        print("ERROR: Vim or Neovim must be above the required version")
 
     return dependencies
 
@@ -128,23 +128,23 @@ def askUserData() -> dict:
     print()
     box_draw("User information")
     print()
-    user["name"] = input(" Full Name 'John Smith': ")
+    user["name"] = input("Full Name 'John Smith': ")
     if user["name"] == "":
         print("Error! Must enter a fist and last name. example: John Smith")
-        quit()
+        exit(1)
     first, *mid, last = user["name"].split()
 
-    user["user"] = input(" Username '{}{}' [Enter] for default: ".format(first[0].lower(), last.lower()))
+    user["user"] = input("Username '{}{}' [Enter] for default: ".format(first[0].lower(), last.lower()))
     if user["user"] == "":
         user["user"] = first[0].lower() + last.lower()
 
-    user["company"] = input(" Company Name: ")
+    user["company"] = input("Company Name: ")
     if user["company"] == "":
         print("Error! Must enter a company name.")
-        quit()
+        exit(2)
 
     user["email"] = input(
-        " Email '{}{}@{}.net [Enter] for default': ".format(
+        "Email '{}{}@{}.net [Enter] for default': ".format(
             first.lower(), last[0].lower(), flat_string(user["company"])
         )
     )
@@ -162,13 +162,13 @@ def askUserData() -> dict:
 def createUserVim(user: dict) -> None:
     if not user:
         return
-    print(" Creating user.vim")
+    print("Creating user.vim")
     f = open("vim/user.vim", "w")
-    f.write("let g:_NAME_    = {}\n".format(user["name"]))
-    f.write("let g:_USER_    = {}\n".format(user["user"]))
-    f.write("let g:_COMPANY_ = {}\n".format(user["company"]))
-    f.write("let g:_EMAIL_   = {}\n".format(user["email"]))
-    f.write("let g:_VIM_     = {}\n".format(user["vim"]))
+    f.write(f"let g:_NAME_    = {user['name']}\n")
+    f.write(f"let g:_USER_    = {user['user']}\n")
+    f.write(f"let g:_COMPANY_ = {user['company']}\n")
+    f.write(f"let g:_EMAIL_   = {user['email']}\n")
+    f.write(f"let g:_VIM_     = {user['vim']}\n")
     f.close()
 
 
@@ -179,7 +179,7 @@ def createUserGit(user: dict) -> None:
         "\n".join(
             [
                 "[user]",
-                f"	name = {user['name']}" if user else "",
+                f"	name  = {user['name']}" if user else "",
                 f"	email = {user['email']}" if user else "",
                 "[core]",
                 f"	editor = {user['vim']}" if user else "",
@@ -187,26 +187,26 @@ def createUserGit(user: dict) -> None:
                 "[help]",
                 "	autocorrect = 1",
                 "[color]",
-                "	ui = auto",
-                "	branch = auto",
-                "	diff = auto",
+                "	ui          = auto",
+                "	branch      = auto",
+                "	diff        = auto",
                 "	interactive = auto",
-                "	status = auto",
-                "	grep = auto",
-                "	pager = true",
-                "	decorate = auto",
-                "	showbranch = auto",
+                "	status      = auto",
+                "	grep        = auto",
+                "	pager       = true",
+                "	decorate    = auto",
+                "	showbranch  = auto",
                 "[push]",
-                "	default = simple",
+                "	default     = simple",
                 "[credential]",
-                "	helper = cache --timeout=28800",
+                "	helper      = cache --timeout=28800",  # Don't ask for a password for 8 hours
                 "[alias]",
-                "	export = archive -o latest.tar.gz -9 --prefix=latest/",
-                "	details = log -n1 -p --format=fuller",
-                r"	logpretty = log --graph --decorate --pretty=format:'%C(yellow)%h%Creset%C(auto)%d%n%Creset %s %C(green)(%cr) %C(blue)<%an>%Creset'",
-                r"	logshort = log --graph --decorate --pretty=format:'%C(yellow)%h%Creset -%C(auto)%h %d%Creset %s %C(green)(%cr) %C(blue)<%an>%Creset' --abbrev-commit",
-                "	stats-commits = git shortlog -sn --no-merges",
-                "	s = status",
+                "	s             = status",
+                "	export        = archive -o latest.tar.gz -9 --prefix=latest/",
+                "	details       = log -n1 -p --format=fuller",
+                r"	logpretty    = log --graph --decorate --pretty=format:'%C(yellow)%h%Creset%C(auto)%d%n%Creset %s %C(green)(%cr) %C(blue)<%an>%Creset'",
+                r"	logshort     = log --graph --decorate --pretty=format:'%C(yellow)%h%Creset -%C(auto)%h %d%Creset %s %C(green)(%cr) %C(blue)<%an>%Creset' --abbrev-commit",
+                "	stats-commits = shortlog -sn --no-merges",  # Shows number of lines / commit by author for the current branch
                 "[pull]",
                 "	ff = only",
                 "",  # Ends in newline
@@ -216,15 +216,14 @@ def createUserGit(user: dict) -> None:
     f.close()
 
 
-def createFolders():
-    dir = SYS_DATA["home"] + "/.config/nvim/"
+def createFolders() -> None:
+    dir = f"{SYS_DATA['home']}/.config/nvim/"
     if os.path.isdir(dir):
         return
-    else:
-        os.makedirs(dir)
+    os.makedirs(dir)
 
 
-def createSysLinks():
+def createSysLinks() -> None:
     """
     {DOT_FILES}/vim -> ~/.vim
     {DOT_FILES}/vim/vimrc -> ~/.vimrc
@@ -232,21 +231,24 @@ def createSysLinks():
     {DOT_FILES}/nvim/init.vim -> ~/.config/nvim/init.vim
 
     """
+    home = SYS_DATA["home"]
+    dot_files = SYS_DATA["script_dir"]
     symlinks = {
-        SYS_DATA["sdir"] + "/vim": SYS_DATA["home"] + "/.vim",
-        SYS_DATA["sdir"] + "/vim/vimrc": SYS_DATA["home"] + "/.vimrc",
-        SYS_DATA["sdir"] + "/tmux/tmux.conf": SYS_DATA["home"] + "/.tmux.conf",
-        SYS_DATA["sdir"] + "/git/gitconfig": SYS_DATA["home"] + "/.gitconfig",
-        SYS_DATA["sdir"] + "/nvim/init.vim": SYS_DATA["home"] + "/.config/nvim/init.vim",
+        f"{dot_files}/vim": f"{home}/.vim",
+        f"{dot_files}/vim/vimrc": f"{home}/.vimrc",
+        f"{dot_files}/tmux/tmux.conf": f"{home}/.tmux.conf",
+        f"{dot_files}/git/gitconfig": f"{home}/.gitconfig",
+        f"{dot_files}/nvim/init.vim": f"{home}/.config/nvim/init.vim",
     }
-    print()
-    print("Creating symlinks")
+    print("\nCreating symlinks")
 
     for src, dest in symlinks.items():
-        if os.path.isfile(dest):
-            os.remove(dest)
-        if os.path.islink(dest):
-            os.unlink(dest)
+        if os.path.isfile(dest):  # File already exists
+            os.remove(dest)  # Del
+        if os.path.islink(dest):  # Link already exists
+            os.unlink(dest)  # Del
+
+        # Create new Link to file
         print(" {} -> {}".format(src, dest))
         os.symlink(src, dest)
 
@@ -272,42 +274,40 @@ def exportDotFiles():
                 print(" Add line: {}".format(exportLine.replace("\n", ""), fileName))
                 appendFile.write(exportLine)
 
-    print()
-    print("Exporting DOT_FILES environment variable")
-    fn = ""
+    print("\nExporting DOT_FILES environment variable")
+    exportLines = [
+        f"export DOT_FILES={SYS_DATA['script_dir']}\n",
+        "export CLICOLOR=1\n",
+        "source $DOT_FILES/shell/autorun.sh\n",
+    ]
+
     if SYS_DATA["os"] == "Darwin":
-        print(" Darwin detected: Selecting file '~/.bash_profile' and '~/.zshrc'")
+        print(" Darwin detected: Appending files '~/.bash_profile' and '~/.zshrc'")
 
-        fn = SYS_DATA["home"] + "/.bash_profile"
-        exportline = "export DOT_FILES=" + SYS_DATA["sdir"] + "\n"
-        autoruncode = "export CLICOLOR=1\nsource $DOT_FILES/shell/autorun.sh\n"
-        safe_append(fn, [exportline, autoruncode])
+        # ~/.bash_profile
+        safe_append(f"{SYS_DATA['home']}/.bash_profile", exportLines)
 
-        fn = SYS_DATA["home"] + "/.zshrc"
-        zshprompt = "prompt='%F{028}%n@%m %F{025}%~%f %% '"
-        safe_append(fn, [exportline, autoruncode, zshprompt])
-    else:
-
+        # ~/.zshrc
         safe_append(
-            f'{SYS_DATA["home"]}/.bashrc',
-            [
-                f'export DOT_FILES={SYS_DATA["sdir"]}\n',
-                "export CLICOLOR=1\n",
-                "source $DOT_FILES/shell/autorun.sh\n",
-            ],
+            f"{SYS_DATA['home']}/.zshrc",
+            [*exportLines, "prompt='%F{028}%n@%m %F{025}%~%f %% '"],
         )
+    else:
+        # ~/.bashrc
+        safe_append(f"{SYS_DATA['home']}/.bashrc", exportLines)
 
 
 def install(skipUser: bool = False):
     user = None
     if not skipUser:
         user = askUserData()
+        createUserVim(user)
 
-    createUserVim(user)
     createUserGit(user)
     createFolders()
     createSysLinks()
     exportDotFiles()
+
     print()
     box_draw("Final Steps")
     print()
@@ -322,15 +322,20 @@ def main():
     """
     Main
     """
-    # process command line arguments
+    # Processor for command line arguments
     parser = argparse.ArgumentParser()
+
+    # Group
     xorgroup = parser.add_mutually_exclusive_group()
     xorgroup.add_argument("-i", "--install", help="Install Files", action="store_true", default=False)
     xorgroup.add_argument("-r", "--remove", help="Remove Files", action="store_true", default=False)
+
     # parser.add_argument('-', help="Column to show", type=int, default=16, dest="col")
     parser.add_argument(
         "--skip-user", help="Install, Skipping user setup", action="store_true", default=False, dest="skipUser"
     )
+
+    # Parse the given args
     args = parser.parse_args()
 
     collect_system_data()
@@ -338,20 +343,18 @@ def main():
 
     if not hasDependencies(skipUser=args.skipUser):
         print("\nError: Missing dependencies.\n")
-        quit()
+        exit(3)
 
     if args.install or args.skipUser:
         install(skipUser=args.skipUser)
+
     elif args.remove:
-        remove()  # TODO: Add remove
-    else:
-        print()
-        box_draw("Help")
-        print(" Usage: ./DotSetup --install")
-        print()
-    return
+        print("TODO: Add Remove")  # TODO: Add remove
+        return
+    # elif args.backup:  # TODO: Add Backup
+    # elif args.restore: # TODO: Add Restore
 
 
 if __name__ == "__main__":
     main()
-    exit(0)
+    exit(0)  # Normal
