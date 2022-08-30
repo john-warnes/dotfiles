c6d113e756eca03a90ee0778d8ac24d62cb29e7d
John Warnes <johnw@gurutechnologies.net>
2022-08-30 15:08:34 -0600

    Create-ssh-user support for random password, and safty check for already existing home folder

--- scripts/create-ssh-user.sh
+++ scripts/create-ssh-user.sh
@@ -1,23 +1,39 @@
 #! /bin/bash
 
+# Script asks for SUDO power
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 # TODO: Add a flag to toggle adding user to SUDO or not
 
 function help {
     cat <<EOF
-    Usage: $0 [username] ["full name"] ["SSH Public Key"]
+    Usage: $0 [-p] [username] ['full name'] ['SSH Public Key']
 
     -h|--help                    Display this text.
+    -p|--password                Asks for user password interactively
+                                 else password will be random '~/password.txt'
 EOF
     exit
 }
+
+# If 1 then ask for the password on command line
+ask_password=0
+force=0
 
 POSITIONAL_ARGS=()
 
 while (("$#")); do
     case "$1" in
-    -? | -h | --help)
+    -\? | -h | --help)
         shift 1
         help
+        ;;
+    -p | --password)
+        shift
+        ask_password=1
+        ;;
+    -f | --force)
+        shift
+        force=1
         ;;
     --) # end argument parsing
         shift
@@ -39,25 +55,56 @@ done
 eval set -- "${POSITIONAL_ARGS[@]}"
 # echo "Postional Args: ${POSITIONAL_ARGS[@]}"
 
-# Check if we have at least one
-[[ ${#POSITIONAL_ARGS[@]} < 3 ]] && help
+# Check if we have at least three
+[[ ${#POSITIONAL_ARGS[@]} < 3 ]] && echo "Error: Invalid arguments" && help
 
 username=${POSITIONAL_ARGS[0]}
 fullname=${POSITIONAL_ARGS[1]}
 pubkey=${POSITIONAL_ARGS[2]}
 
-echo "Adding user: $username"
-echo ""
+if [[ $force == 0 ]] && [ -d "/home/$username" ]; then
+    echo "Error: User home directory already exists. (-f to force)"
+    exit 1
+fi
 
-sudo adduser $username --gecos "$fullname,,,,"
+echo "New Username: $username"
 
-sudo mkdir /home/$username/.ssh
-sudo touch /home/$username/.ssh/authorized_keys
-echo $pub_key | sudo tee -a /home/$username/.ssh/authorized_keys
-sudo chmod 755 /home/$username/.ssh
-sudo chmod 664 /home/$username/.ssh/authorized_keys
-sudo chown -R $username:$username /home/$username/
+if [[ $ask_password == 1 ]]; then
+    # Ask for password for new user
+    read -s -p "New Password: " password
+    echo ""
+fi
 
-echo "Sudo-ing user: $username"
-sudo usermod -a -G sudo $username
-echo "Done."
+# If password is empty set it to random
+if [ -z "$password" ]; then
+    password=$(head /dev/urandom | LC_ALL=C tr -dc A-Za-z0-9 | head -c 13)
+    echo "Random password generated: '$password'"
+fi
+
+# Create the new user and set the password
+#sudo adduser --disabled-password --gecos "$fullname,,,," $username
+#echo "$username:$password" | sudo chpasswd
+
+# Save the users password into password.txt
+echo "$password" > /home/$username/password.txt
+sudo chown $username:$username /home/$username/password.txt
+sudo chmod 600 /home/$username/password.txt
+echo "Password saved to '/home/$username/password.txt'"
+
+
+# Interactively asks for password
+# sudo adduser --gecos "$fullname,,,," $username
+
+exit 0
+
+
+# sudo mkdir /home/$username/.ssh
+# sudo touch /home/$username/.ssh/authorized_keys
+# echo $pub_key | sudo tee -a /home/$username/.ssh/authorized_keys
+# sudo chmod 755 /home/$username/.ssh
+# sudo chmod 664 /home/$username/.ssh/authorized_keys
+# sudo chown -R $username:$username /home/$username/
+
+# echo "Sudo-ing user: $username"
+# sudo usermod -a -G sudo $username
+# echo "Done."
