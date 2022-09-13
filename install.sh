2aa49e38a755a283d784a9d2a0f8797f4de0b3fb
John Warnes <johnw@gurutechnologies.net>
2022-09-13 16:10:42 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-# Run the python setup
+if [[ "/bin/bash" == "$SHELL" ]]; then
+    pushd $(dirname -- "${BASH_SOURCE[0]}")
+else
+    pushd $(dirname -- "$0")
+fi
+
+# Setup without asking for user info
 python3 ~/dotfiles/DotSetup.py --skip-user
+
+# Run interactively set asking for user info
+# python3 ~/dotfiles/DotSetup.py --install
+
+popd
