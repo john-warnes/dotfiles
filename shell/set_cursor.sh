37745cdef6a7d1206ca40e01d26b5fa02a466391
John Warnes <john@savagebee.org>
2026-04-12 03:14:31 -0600

    shell: switch cursor to blinking beam (set_cursor.sh)

--- shell/set_cursor.sh
+++ shell/set_cursor.sh
@@ -5,9 +5,9 @@
 # echo -ne "\e[2 q"    # Steady block █
 # echo -ne "\e[3 q"    # Blinking underline
 # echo -ne "\e[4 q"    # Steady underline
-# echo -ne "\e[5 q"    # Blinking vertical bar (beam)
+echo -ne "\e[5 q"    # Blinking vertical bar (beam)
 # echo -ne "\e[6 q"    # Steady vertical bar |
 
 # echo -ne "\e[2 q"    # Steady block █
-echo -ne "\e[6 q"    # Steady vertical bar |
+# echo -ne "\e[6 q"    # Steady vertical bar |
 
